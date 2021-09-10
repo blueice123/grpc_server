1927016bce8b111eace4e9084e7e5c7e477f5b3d
하수용 <syha@megazone.com>
2021-09-10 16:47:58 +0900

    change code

--- grpc/examples/python/helloworld/greeter_server.py
+++ grpc/examples/python/helloworld/greeter_server.py
@@ -19,12 +19,13 @@ import logging
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
+import socket
 
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
     def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello server-1, %s!' % request.name)
+        return helloworld_pb2.HelloReply(message='Hello, %s!' % socket.gethostname())
 
 
 def serve():
